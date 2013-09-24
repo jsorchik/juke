e8718cf715262a4c41856f17b314bf8ee840a9f0
jsorchik <jsorchik@gmail.com>
2013-09-24 13:36:53 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Juke::Application.config.secret_token = 'c16938b52e26a029c62dfb717729907347751ead5892e6470bf0831e98904d424dd6f9c09eb5fff6da038989459e3209f3137572175c3652269598e0a5c91bbd'
